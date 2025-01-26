ec0729a985845c871041e12d328f0f28771857c6
salomefaure <salome.faure79@gmail.com>
2025-01-26 16:36:37 +0100

    ajout de style page skills

--- src/main/java/fr/skillup/controllers/PeopleController.java
+++ src/main/java/fr/skillup/controllers/PeopleController.java
@@ -1,11 +1,19 @@
 package fr.skillup.controllers;
 
 import fr.skillup.core.controller.Controller;
+import fr.skillup.core.model.Model;
+import fr.skillup.models.PersonModel;
+import fr.skillup.models.SkillModel;
 
 public class PeopleController extends Controller {
     @Override
     public void init() {
         this.render("people_view", this.params);
 
+    }
+
+    public String getPeople() {
+        PersonModel model = Model.get(PersonModel.class);
+        return model.getPersons().toJson();
     }
 }
--- src/main/java/fr/skillup/models/PersonModel.java
+++ src/main/java/fr/skillup/models/PersonModel.java
@@ -3,10 +3,12 @@ package fr.skillup.models;
 import fr.skillup.core.database.Result;
 import fr.skillup.core.model.Model;
 
+import java.sql.Date;
+
 public class PersonModel extends Model {
 
     public Result getPersons() {
-        return this.select("SELECT id, firstname, lastname FROM person", Integer.class, String.class, String.class);
+        return this.select("SELECT id, firstname, lastname, entry_date, job, picture FROM person", Integer.class, String.class, String.class, Date.class, String.class, String.class);
     }
 
 }
