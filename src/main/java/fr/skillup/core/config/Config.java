4d96698d2e699d41674477655860f8205a014fee
StayZe <blandin.ilann@gmail.com>
2025-01-29 14:27:49 +0100

    Correction erreurs sonarqube

--- src/main/java/fr/skillup/core/config/Config.java
+++ src/main/java/fr/skillup/core/config/Config.java
@@ -1,6 +1,7 @@
 package fr.skillup.core.config;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Objects;
 import java.util.Properties;
 import java.util.logging.Logger;
@@ -9,18 +10,31 @@ public class Config {
 
     private static Properties properties;
 
+    private Config() {
+        throw new IllegalStateException("Classe utilitaire");
+    }
+
     /**
      * Charge le fichier de configuration
      */
     public static void load() {
         Config.properties = new Properties();
+        InputStream stream = null;
         try {
-            Config.properties.load(Objects.requireNonNull(Config.class.getResource("/config.properties")).openStream());
+            stream = Objects.requireNonNull(Config.class.getResource("/config.properties")).openStream();
+            Config.properties.load(stream);
         } catch (IOException e) {
             Logger.getLogger(Config.class.getName()).severe(e.getMessage());
         } finally {
             if (Config.properties.isEmpty()) {
                 Logger.getLogger(Config.class.getName()).severe("Le fichier de configuration n'a pas pu être chargé");
+            }
+            if (stream != null) {
+                try {
+                    stream.close();
+                } catch (IOException e) {
+                    Logger.getLogger(Config.class.getName()).severe(e.getMessage());
+                }
             }
         }
     }
