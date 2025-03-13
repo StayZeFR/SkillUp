56ad34b04536e92e5a6f8c8e76e1403d506dbc06
anasoueldlaaboura <anasoueld0412@gmail.com>
2025-03-13 19:28:13 +0100

    mise à jour branche anas

--- src/main/java/fr/skillup/controllers/PeopleController.java
+++ src/main/java/fr/skillup/controllers/PeopleController.java
@@ -4,10 +4,17 @@ import fr.skillup.core.controller.Controller;
 import fr.skillup.core.model.Model;
 import fr.skillup.models.PersonModel;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PeopleController extends Controller {
+    private static final String VIEW = "people";
     @Override
     public void init() {
-        this.render("people_view", this.params);
+        System.out.println("PeopleController.init() called from Bridge");
+        Map<String, Object> params = new HashMap<>();
+        params.put("view", PeopleController.VIEW);
+        super.render("people_view", params);
     }
 
     public String getPeople() {
