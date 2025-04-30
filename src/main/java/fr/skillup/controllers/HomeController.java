81db718625d19c0bce52c526dc9ba597b59ae263
StayZe <blandin.ilann@gmail.com>
2025-04-30 14:51:52 +0200

    inports

--- src/main/java/fr/skillup/controllers/HomeController.java
+++ src/main/java/fr/skillup/controllers/HomeController.java
@@ -1,13 +1,8 @@
 package fr.skillup.controllers;
 
 import fr.skillup.core.controller.Controller;
-import fr.skillup.core.database.Result;
-import fr.skillup.core.model.Model;
-import fr.skillup.models.MissionModel;
-import fr.skillup.models.PersonModel;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class HomeController extends Controller {
--- src/main/java/fr/skillup/core/bridge/Bridge.java
+++ src/main/java/fr/skillup/core/bridge/Bridge.java
@@ -12,7 +12,6 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.atomic.AtomicReference;
 import java.util.logging.Logger;
 
 public class Bridge {
